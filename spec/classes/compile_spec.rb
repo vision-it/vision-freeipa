2151b266b071b5999166a58facf878818c462423
Jack Henschel <jackdev@mailbox.org>
2019-08-20 09:59:03 +0200

    Delete obsolete code from freeipa replica

--- spec/classes/compile_spec.rb
+++ spec/classes/compile_spec.rb
@@ -8,24 +8,6 @@ describe 'vision_freeipa' do
         facts
       end
 
-      context 'include replica' do
-        let(:pre_condition) { 'include vision_docker' }
-
-        let :params do
-          {
-            type: 'replica'
-
-          }
-        end
-        context 'compile' do
-          it { is_expected.to compile.with_all_deps }
-        end
-
-        context 'contains' do
-          it { is_expected.to contain_class('vision_freeipa::replica') }
-        end
-      end
-
       context 'include master' do
         let :pre_condition do
           [
